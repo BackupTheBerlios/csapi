c79b73ed898f68b07f7ecf5ba27b10d25242d18d
grandpas <grandpas>
2006-10-22 20:03:14 +0000

    Format source.

--- csplugin/src/org/csapi/csplugin/jobs/GetReportJob.java
+++ csplugin/src/org/csapi/csplugin/jobs/GetReportJob.java
@@ -21,79 +21,90 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
 /**
- * <p>The Job class for the Get Report command in the global menu.</p>
+ * <p>
+ * The Job class for the Get Report command in the global menu.
+ * </p>
  * 
- * <p>It prompts for query and attributes (in the current thread workspace,
- * then get the default sessionMgr and execute the query. When result is
- * returned, the view's viewer is updated.</p>
+ * <p>
+ * It prompts for query and attributes (in the current thread workspace, then
+ * get the default sessionMgr and execute the query. When result is returned,
+ * the view's viewer is updated.
+ * </p>
  * 
  * @author Boris Baldassari
- *
+ * 
  */
 public class GetReportJob extends Job {
 
-public GetReportJob(String name) {
-super(name);
-}
+	public GetReportJob(String name) {
+		super(name);
+	}
 
-/* (non-Javadoc)
- * @see org.eclipse.core.runtime.jobs.Job#run(org.eclipse.core.runtime.IProgressMonitor)
- */
-protected IStatus run(IProgressMonitor monitor) {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.jobs.Job#run(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	protected IStatus run(IProgressMonitor monitor) {
 
-        // Get the query to be executed.
-        JFrame myJFrame = new JFrame();
-        final String query = (String) JOptionPane.showInputDialog(myJFrame,
-                "Please provide query:", "Query",
-                JOptionPane.OK_CANCEL_OPTION, null, null, 
-                "(product_name='Product A')");
-        // if cancel is hit
-        if (query == null) { 
-            return new Status(Status.CANCEL, "CSPlugin", 0, 
-            "Query cancelled", null); 
-            }
-        
-        // Get the list of attributes.
-        final String attributesList = (String) JOptionPane.showInputDialog(myJFrame,
-                "Please provide attributes list, separated by pipes:", "Attributes needed",
-                JOptionPane.OK_CANCEL_OPTION, null, null, "problem_number|problem_synopsis");
-        // if cancel is hit
-        if (attributesList == null) { 
-            return new Status(Status.CANCEL, "CSPlugin", 0, 
-            "Attributes cancelled", null); 
-            }
+		// Get the query to be executed.
+		JFrame myJFrame = new JFrame();
+		final String query = (String) JOptionPane.showInputDialog(myJFrame,
+				"Please provide query:", "Query", JOptionPane.OK_CANCEL_OPTION,
+				null, null, "(product_name='Product A')");
+		// if cancel is hit
+		if (query == null) {
+			return new Status(Status.CANCEL, "CSPlugin", 0, "Query cancelled",
+					null);
+		}
 
-Display.getDefault().asyncExec(new Runnable() { public void run() {
-    try {
-SessionMgr sessionMgr = SessionMgr.getDefault();
+		// Get the list of attributes.
+		final String attributesList = (String) JOptionPane.showInputDialog(
+				myJFrame,
+				"Please provide attributes list, separated by pipes:",
+				"Attributes needed", JOptionPane.OK_CANCEL_OPTION, null, null,
+				"problem_number|problem_synopsis");
+		// if cancel is hit
+		if (attributesList == null) {
+			return new Status(Status.CANCEL, "CSPlugin", 0,
+					"Attributes cancelled", null);
+		}
 
-Report report;
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
+				try {
+					SessionMgr sessionMgr = SessionMgr.getDefault();
 
-try {
-report = sessionMgr.getReport(query, attributesList);
-} catch (PluginException pe) {
-report = null;
-JOptionPane.showMessageDialog(null, pe.getMessage());
-}
+					Report report;
 
-        IWorkbenchWindow wkbch = PlatformUI.getWorkbench().getWorkbenchWindows()[0];
-        IViewPart inst = null;
-        
-        // Get the instance of the view and focus it.
-        inst = wkbch.getActivePage().showView("org.csapi.csplugin.views.ShowReportView");
-        if (inst != null && report != null) {
-            ((ShowReportView) inst).setColumns(report.getAttributes());
-            ((ShowReportView) inst).setInput(report);
-            ((ShowReportView) inst).setFocus();
-        }
-    } catch (PartInitException e) {
-        e.printStackTrace();
-    }
-}
-});
+					try {
+						report = sessionMgr.getReport(query, attributesList);
+					} catch (PluginException pe) {
+						report = null;
+						JOptionPane.showMessageDialog(null, pe.getMessage());
+					}
 
-return new Status(Status.OK, "CSPlugin", 0,
-"Get Report successfull.", null);
-}
+					IWorkbenchWindow wkbch = PlatformUI.getWorkbench()
+							.getWorkbenchWindows()[0];
+					IViewPart inst = null;
+
+					// Get the instance of the view and focus it.
+					inst = wkbch.getActivePage().showView(
+							"org.csapi.csplugin.views.ShowReportView");
+					if (inst != null && report != null) {
+						((ShowReportView) inst).setColumns(report
+								.getAttributes());
+						((ShowReportView) inst).setInput(report);
+						((ShowReportView) inst).setFocus();
+					}
+				} catch (PartInitException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		return new Status(Status.OK, "CSPlugin", 0, "Get Report successfull.",
+				null);
+	}
 
 }
--- csplugin/src/org/csapi/csplugin/jobs/LoginJob.java
+++ csplugin/src/org/csapi/csplugin/jobs/LoginJob.java
@@ -11,36 +11,39 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 
 /**
- * <p>The job class for the Login command in the global menu.</p>
+ * <p>
+ * The job class for the Login command in the global menu.
+ * </p>
  * 
- * <p>From the csapicore point of view, this is done with the login() method
- * from the sessionMgr. The singleton pattern implementation allows to retrieve 
- * the sessionMgr instance easily from static methods in the sessionMgr 
- * class.</p>
+ * <p>
+ * From the csapicore point of view, this is done with the login() method from
+ * the sessionMgr. The singleton pattern implementation allows to retrieve the
+ * sessionMgr instance easily from static methods in the sessionMgr class.
+ * </p>
  * 
  * @author Boris Baldassari
  */
 public class LoginJob extends Job {
 
-public LoginJob(String name) {
-super(name);
-}
+	public LoginJob(String name) {
+		super(name);
+	}
 
-/*
- * (non-Javadoc)
- * 
- * @see org.eclipse.core.internal.jobs.InternalJob#run(org.eclipse.core.runtime.IProgressMonitor)
- */
-protected IStatus run(IProgressMonitor monitor) {
-SessionMgr sessionMgr = SessionMgr.getDefault();
-try {
-sessionMgr.login();
-} catch (PluginException pe) {
-//JOptionPane.showMessageDialog(null, pe.getMessage());
-return new Status(Status.ERROR, "CSplugin", 0, pe.getMessage(),
-null);
-}
-return new Status(Status.OK, "CSplugin", 0,
-"Connection successful.", null);
-}
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.internal.jobs.InternalJob#run(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	protected IStatus run(IProgressMonitor monitor) {
+		SessionMgr sessionMgr = SessionMgr.getDefault();
+		try {
+			sessionMgr.login();
+		} catch (PluginException pe) {
+			// JOptionPane.showMessageDialog(null, pe.getMessage());
+			return new Status(Status.ERROR, "CSplugin", 0, pe.getMessage(),
+					null);
+		}
+		return new Status(Status.OK, "CSplugin", 0, "Connection successful.",
+				null);
+	}
 }
